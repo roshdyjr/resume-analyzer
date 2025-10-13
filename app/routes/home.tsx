4f0476efbfac2700f09bb5dbc13456fa58b6a053
roshdyjr <ahmedrush96@gmail.com>
2025-10-13 14:36:48 +0300

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,34 @@
+import Navbar from "~/components/Navbar";
+import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "Resmue Analyzer" },
+    { name: "description", content: "Test your resume for your dream job!" },
+  ];
+}
+
+export default function Home() {
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+
+
+
+  </main>;
+}
